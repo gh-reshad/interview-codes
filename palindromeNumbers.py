d3de911a059cab374a549c874b1f611b7c895bc9
gh-reshad <ghazal.reshad@uoit.net>
2019-10-19 23:22:31 -0400

    'updates'

--- palindromeNumbers.py
+++ palindromeNumbers.py
@@ -5,14 +5,20 @@ def is_palindromic(x):
     
     if x is not None:
         x = x.__str__()
+        y = np.zeros((len(x)))
         last = len(x) - 1
+        idx = len(x)
         if x[0] == x[last]:
-            return True
+            for i in range(len(x)):
+                y[i] = x[(idx-1) - i]
+            return y
         else:
             return False
+        
     else:
         print('empty variable')
 
-number = None
+number =  121
+# is_palindromic(number)
 output = is_palindromic(number)
 print(output)
--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,18 +1,25 @@
 import sys
+import numpy as np
 
 
 for lines in sys.stdin:
     input = lines
     tokens = input.split()
-    print(tokens)
-    str = ''.join(x for x in tokens)
+    #print(tokens)
+    #print(type(tokens))
+    str = np.asarray(tokens)
+    #str = ''.join(x for x in tokens)
     print(str)
     max = 0
     arr = []
-    for i in range(len(str)):
-        arr.append(str[i])
-
+    k = 0
+    while k < len(str):
+        arr.append(str[k])
+        k += 2
+    
     print(arr)
+ 
+   
    
     #     diff = int(str[i+1]) - int(str[i])
     #     if diff >= max:
