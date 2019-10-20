5bdd284303a39e491d41c276cb08e75f7fe36d0e
gh-reshad <ghazal.reshad@uoit.net>
2019-10-20 11:06:28 -0400

    'working'

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -3,28 +3,19 @@ import numpy as np
 
 
 for lines in sys.stdin:
-    input = lines
-    tokens = input.split()
-    #print(tokens)
-    #print(type(tokens))
-    str = np.asarray(tokens)
-    #str = ''.join(x for x in tokens)
-    print(str)
+    input = lines[:-1]
+    tokens = input.split(',')
     max = 0
-    arr = []
     k = 0
-    while k < len(str):
-        arr.append(str[k])
-        k += 2
-    
-    print(arr)
- 
-   
-   
-    #     diff = int(str[i+1]) - int(str[i])
-    #     if diff >= max:
-    #         max = diff
-    # print(max)
+    while k < len(tokens):
+        if k+1 < len(tokens):
+            diff = int(tokens[k+1]) - int(tokens[k])
+            if diff >= max:
+                max = diff
+        k += 1
+
+    print(max)
+
     # sum = 0
     # for i in range(len(str)):
     #     sum += int(str[i])
