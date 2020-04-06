8d83cacd9b5aecde01192c1e551e41369e7b0fcc
gh-reshad <ghazal.reshad@uoit.net>
2020-04-06 18:26:53 -0400

    'update'

---
+++ reduced_string.py
@@ -0,0 +1,40 @@
+import numpy as np
+import math
+import os
+import random
+import sys
+
+def superReducedString(s):
+    arr = np.zeros(26,)
+    reduced_arr = []
+    for i in range(len(s)):
+        var = ord(s[i]) - 97
+        arr[var] += 1
+        
+ 
+    print(arr)
+    for j in range(len(s)):
+        if arr[j] % 2 == 0:
+            letter = chr(j + 97)
+            if letter in s:
+                pass
+        elif arr[j] % 2 == 1:
+            letter = chr(j + 97)
+            if letter in s:
+                reduced_arr.append(letter)                
+                
+    if len(reduced_arr) == 0:
+        return 'empry string'
+    else:
+        return reduced_arr
+    
+    
+
+
+    
+if __name__ == '__main__':
+
+    s = input().rstrip().split(' ')
+    result = superReducedString(s)
+    
+    print(result)
