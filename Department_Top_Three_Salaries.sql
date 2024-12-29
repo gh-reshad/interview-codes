5304ecccb683ba367cca2c88b2b311820b3660b3
gh-reshad <reshad.ghazal@yahoo.com>
2024-12-29 10:32:51 -0500

    Hard sql question solved!

---
+++ Department_Top_Three_Salaries.sql
@@ -0,0 +1,50 @@
+-- Table: Employee
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | id           | int     |
+-- | name         | varchar |
+-- | salary       | int     |
+-- | departmentId | int     |
+-- +--------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- departmentId is a foreign key (reference column) of the ID from the Department table.
+-- Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+-- Table: Department
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the ID of a department and its name.
+ 
+
+-- A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+-- Write a solution to find the employees who are high earners in each of the departments.
+
+-- Return the result table in any order.
+
+with salaryRank as 
+(
+    select id,
+    name, 
+    salary,
+    departmentId,
+    DENSE_RANK() over (PARTITION BY departmentId ORDER BY salary DESC) as salary_rank
+    from Employee 
+)
+select d.name as Department,
+sr.name as Employee,
+sr.salary as Salary
+from salaryRank sr
+inner join Department d
+on sr.departmentId = d.id
+where sr.salary_rank <= 3
+order by sr.name
